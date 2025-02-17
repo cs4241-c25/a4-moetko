6f489de31aa78cff9524a04e9721399af22b7985
Moet <moetodonnell@gmail.com>
2025-02-17 13:21:33 -0500

    Edited to use with Vercel

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,11 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "./",
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000", // Local development proxy
+        target: "http://localhost:3000",
         changeOrigin: true,
       },
       "/auth": {
